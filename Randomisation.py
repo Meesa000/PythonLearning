29320252056b2f1164c61f0d1bdd7e0016a7304f
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2022-11-12 20:37:28 +0000

    Randomisation

---
+++ Randomisation.py
@@ -0,0 +1,28 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️","⬜️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+position = int(position)
+row_len = len(map)
+map[row_len] = "X"
+
+print(map)
+
+
+
+#use the len (aka index) so that when user enters int
+#it replaces square with x
+#have to find out how to rweplace item i nlist
+
+
+
+
+
+#Write your code above this row 👆
+
