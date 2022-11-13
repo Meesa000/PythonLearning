09746b8d59e9b15f8e020d8edc2410e22ffe60a3
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2022-11-13 11:45:01 +0000

    rok p s

--- Randomisation.py
+++ Randomisation.py
@@ -1,24 +1,37 @@
-# 🚨 Don't change the code below 👇
-row1 = ["⬜️","️⬜️","️⬜️"]
-row2 = ["⬜️","⬜️","️⬜️"]
-row3 = ["⬜️","⬜️","⬜️️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-# 🚨 Don't change the code above 👆
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-#Write your code below this row 👇
-print("github test2222")
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+#make list of rock paper and scisssor
+#make variable for computer turn =  random.choice(list)
+#player turn = input enter rock paper or scissors
+#if statements for rules
+#rock wins vs scissors
+#scissors wins vs paper
+#paper wins vs rock
 
-
-#use the len (aka index) so that when user enters int
-#it replaces square with x
-#have to find out how to rweplace item i nlist
-
-
-
-
-
-#Write your code above this row 👆
-
+#i.e if player turn is rock print(rock)
+#if computer turn == paper, player loses
+#if computer turn == scissors, pc loses
