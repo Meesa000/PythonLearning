4bab9836aeab2d770616f881993ca8b5ea7e464d
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2022-11-13 12:55:14 +0000

    rok p s

--- Randomisation.py
+++ Randomisation.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,8 +25,44 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-#make list of rock paper and scisssor
-#make variable for computer turn =  random.choice(list)
+
+moves = ["Rock", "Paper", "Scissors"] # List of moves
+images = [rock, paper, scissors]
+computer_turn = random.choice(moves) # Computer move, random choice from the moves list
+player_turn = int(input("Enter rock, paper or scissors: ")) # User input for move
+
+
+print(f"You chose {player_turn}") # Print what player and computer chose
+print(images[player_turn])
+print(f"The computer chose {computer_turn}")
+print(images[computer_turn])
+
+if player_turn == computer_turn.lower():
+    print("It's a tie!")
+
+if player_turn == "rock": # If player chooses rock conditionals
+    if computer_turn == "Paper":
+        print("The computer won!")
+    elif computer_turn == "Scissors":
+        print("You won!")
+
+if player_turn == "paper": # If player chooses paper conditionals
+    if computer_turn == "Scissors":
+        print("The computer won!")
+    elif computer_turn == "Rock":
+        print("You won!")
+
+if player_turn == "scissors": # If player chooses scissors conditionals
+    if computer_turn == "Rock":
+        print("The computer won!")
+    elif computer_turn == "Paper":
+        print("You won!")
+
+
+
+
+
+
 #player turn = input enter rock paper or scissors
 #if statements for rules
 #rock wins vs scissors
